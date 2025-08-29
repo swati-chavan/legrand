dbeb85468c8c92b2abc57adde64e556cf03431ca
Swatee <swatee.chavan@gmail.com>
2025-08-29 15:30:08 +0530

    updated code

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -299,7 +299,7 @@ const handleSubmit = async (e) => {
     setButtonText('Processing...'); // Change button text to "Processing..."
 
 
-  await new Promise(resolve => setTimeout(() => resolve(), 2000)); // Directly resolve inside setTimeout
+  await new Promise(resolve => setTimeout(() => resolve(), 2000)); // Directly resolve inside setTimeout test
 
   const isUpdate = !!formData._id;
 
